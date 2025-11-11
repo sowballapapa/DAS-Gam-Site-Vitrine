a24748e582e5e0e9ff5a35e8cec31c5b566f98f1
Papa Balla SOW <sowballapapa@gmail.com>
2025-11-11 21:21:12 +0000

    Ajout de l'emploi temps

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,9 +1,9 @@
-import { Card } from "./ui/card";
+import {Card, CardContent, CardHeader, CardTitle} from "./ui/card";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Label } from "./ui/label";
-import { MapPin, Phone, Mail } from "lucide-react";
+import {MapPin, Phone, Mail, Calendar1, Calendar} from "lucide-react";
 import { useState } from "react";
 
 export function ContactSection() {
@@ -48,6 +48,7 @@ export function ContactSection() {
     },
   ];
 
+
   return (
     <section id="contact" className="py-20 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -88,72 +89,102 @@ export function ContactSection() {
 
             {/* Map */}
             <Card className="bg-gray-100 h-64 flex items-center justify-center">
-              <div className="text-center text-[#666666]">
-                <MapPin className="mx-auto mb-2 text-[#003366]" size={48} />
-                <p>Carte Google Maps</p>
-                <p className="text-sm">Dakar, Sénégal</p>
-              </div>
+                <div className="text-center text-[#666666]">
+                    <MapPin className="mx-auto mb-2 text-[#003366]" size={48}/>
+                    <p>Carte Google Maps</p>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3857.7485271730307!2d-17.3883019!3d14.783189600000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xec10c7ee73fffff%3A0x64c7907d104949e8!2sSchool%20Hamo%204!5e0!3m2!1sen!2ssn!4v1762889677504!5m2!1sen!2ssn"
+                         loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </Card>
           </div>
 
-          {/* Contact Form */}
-          <Card className="bg-white p-8 border-2 border-gray-100">
-            <form onSubmit={handleSubmit} className="space-y-6">
-              <div>
-                <Label htmlFor="name" className="text-[#003366]">
-                  Nom complet
-                </Label>
-                <Input
-                  id="name"
-                  name="name"
-                  type="text"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                  className="mt-2"
-                  placeholder="Votre nom"
-                />
-              </div>
+           <div className="flex flex-col gap-2">
+               {/* Contact Form */}
+               <Card className="bg-white p-8 border-2 border-gray-100">
+                   <form onSubmit={handleSubmit} className="space-y-6">
+                       <div>
+                           <Label htmlFor="name" className="text-[#003366]">
+                               Nom complet
+                           </Label>
+                           <Input
+                               id="name"
+                               name="name"
+                               type="text"
+                               value={formData.name}
+                               onChange={handleChange}
+                               required
+                               className="mt-2"
+                               placeholder="Votre nom"
+                           />
+                       </div>
 
-              <div>
-                <Label htmlFor="email" className="text-[#003366]">
-                  Email
-                </Label>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                  className="mt-2"
-                  placeholder="votre@email.com"
-                />
-              </div>
+                       <div>
+                           <Label htmlFor="email" className="text-[#003366]">
+                               Email
+                           </Label>
+                           <Input
+                               id="email"
+                               name="email"
+                               type="email"
+                               value={formData.email}
+                               onChange={handleChange}
+                               required
+                               className="mt-2"
+                               placeholder="votre@email.com"
+                           />
+                       </div>
 
-              <div>
-                <Label htmlFor="message" className="text-[#003366]">
-                  Message
-                </Label>
-                <Textarea
-                  id="message"
-                  name="message"
-                  value={formData.message}
-                  onChange={handleChange}
-                  required
-                  className="mt-2 min-h-[150px]"
-                  placeholder="Votre message..."
-                />
-              </div>
+                       <div>
+                           <Label htmlFor="message" className="text-[#003366]">
+                               Message
+                           </Label>
+                           <Textarea
+                               id="message"
+                               name="message"
+                               value={formData.message}
+                               onChange={handleChange}
+                               required
+                               className="mt-2 min-h-[150px]"
+                               placeholder="Votre message..."
+                           />
+                       </div>
 
-              <Button
-                type="submit"
-                className="w-full bg-[#DC143C] hover:bg-[#B01030] text-white"
-              >
-                Envoyer le message
-              </Button>
-            </form>
-          </Card>
+                       <Button
+                           type="submit"
+                           className="w-full bg-[#DC143C] hover:bg-[#B01030] text-white"
+                       >
+                           Envoyer le message
+                       </Button>
+                   </form>
+               </Card>
+               {/* Business Hours */}
+               <Card className="text-[#003366] bg-gray-50">
+                   <CardHeader>
+                       <CardTitle className="flex gap-2 text-lg">
+                           <Calendar/>
+                           Horaires d'ouverture
+                       </CardTitle>
+                   </CardHeader>
+                   <CardContent className="text-[#666666] ">
+                       <div className="space-y-2 text-sm">
+                           <div className="flex justify-between">
+                               <span>Lundi - Vendredi</span>
+                               <span className="">8h00 - 18h00</span>
+                           </div>
+                           <div className="flex justify-between">
+                               <span>Samedi</span>
+                               <span className="">9h00 - 14h00</span>
+                           </div>
+                           <div className="flex justify-between">
+                               <span>Dimanche</span>
+                               <span className="">Fermé</span>
+                           </div>
+                       </div>
+                   </CardContent>
+               </Card>
+           </div>
         </div>
       </div>
     </section>
