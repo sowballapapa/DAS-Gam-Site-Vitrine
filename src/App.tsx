8e96f3934086cf65ecdeca9e7dea15be55f0d1e2
Papa Balla SOW <sowballapapa@gmail.com>
2025-11-17 17:29:53 +0000

    Adding logo to google search results

--- src/App.tsx
+++ src/App.tsx
@@ -14,32 +14,40 @@ export default function App() {
           <div>
               <Helmet>
                   {/* Title */}
-                  <title>GAM – Générale Activité Mondiale | BTP, Location Automobile, Transport & Fournitures au Sénégal</title>
+                  <title>GAM – Générale Activité Mondiale | BTP, Location Automobile, Transport & Fournitures au
+                      Sénégal</title>
 
                   {/* Meta Description */}
-                  <meta name="description" content="Fondée en 2006 à Dakar, GAM – Générale Activité Mondiale est une entreprise sénégalaise spécialisée dans le BTP, la location automobile, le transport, l’électricité bâtiment et les fournitures. Votre partenaire de confiance au Sénégal." />
+                  <meta name="description"
+                        content="Fondée en 2006 à Dakar, GAM – Générale Activité Mondiale est une entreprise sénégalaise spécialisée dans le BTP, la location automobile, le transport, l’électricité bâtiment et les fournitures. Votre partenaire de confiance au Sénégal."/>
+                  <meta property="og:image" content="https://www.gamondial.com/assets/logo_gam-CUXMMUsE.jpg"/>
 
                   {/* Google search */}
-                  <meta name="google-site-verification" content="j2YSQfuQXYduoB2fKh1qhmGWHmtIRTvRr9T4WNBurrI" />
+                  <meta name="google-site-verification" content="j2YSQfuQXYduoB2fKh1qhmGWHmtIRTvRr9T4WNBurrI"/>
 
                   {/* Keywords */}
-                  <meta name="keywords" content="GAM, Générale Activité Mondiale, BTP Sénégal, location automobile Dakar, transport Sénégal, électricité bâtiment, fournitures, logistiques, entreprise sénégalaise, services Dakar, Thiés" />
+                  <meta name="keywords"
+                        content="GAM, Générale Activité Mondiale, BTP Sénégal, location automobile Dakar, transport Sénégal, électricité bâtiment, fournitures, logistiques, entreprise sénégalaise, services Dakar, Thiés"/>
 
                   {/* Author */}
-                  <meta name="author" content="GAM - Générale Activité Mondiale" />
+                  <meta name="author" content="GAM - Générale Activité Mondiale"/>
 
                   {/* Open Graph */}
-                  <meta property="og:title" content="GAM – Générale Activité Mondiale | BTP, Location Automobile & Transport au Sénégal" />
-                  <meta property="og:description" content="Entreprise sénégalaise multisectorielle fondée en 2006 à Dakar. BTP, location automobile, transport, électricité bâtiment et fournitures." />
-                  <meta property="og:url" content="https://www.gamondial.com/" />
-                  <meta property="og:type" content="website" />
-                  <meta property="og:image" content="https://www.gamondial.com/logo_gam.jpg" />
+                  <meta property="og:title"
+                        content="GAM – Générale Activité Mondiale | BTP, Location Automobile & Transport au Sénégal"/>
+                  <meta property="og:description"
+                        content="Entreprise sénégalaise multisectorielle fondée en 2006 à Dakar. BTP, location automobile, transport, électricité bâtiment et fournitures."/>
+                  <meta property="og:url" content="https://www.gamondial.com/"/>
+                  <meta property="og:type" content="website"/>
+                  <meta property="og:image" content="https://www.gamondial.com/logo_gam.jpg"/>
 
                   {/* Twitter Card */}
-                  <meta name="twitter:card" content="summary_large_image" />
-                  <meta name="twitter:title" content="GAM – Générale Activité Mondiale | BTP & Location Auto au Sénégal" />
-                  <meta name="twitter:description" content="Entreprise sénégalaise spécialisée en BTP, transport et services polyvalents. Expertise et fiabilité depuis 2006." />
-                  <meta name="twitter:image" content="https://www.gamondial.com/logo_gam.jpg" />
+                  <meta name="twitter:card" content="summary_large_image"/>
+                  <meta name="twitter:title"
+                        content="GAM – Générale Activité Mondiale | BTP & Location Auto au Sénégal"/>
+                  <meta name="twitter:description"
+                        content="Entreprise sénégalaise spécialisée en BTP, transport et services polyvalents. Expertise et fiabilité depuis 2006."/>
+                  <meta name="twitter:image" content="https://www.gamondial.com/logo_gam.jpg"/>
               </Helmet>
               <div className="min-h-screen">
                   <Toaster />
